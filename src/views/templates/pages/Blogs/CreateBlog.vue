b279d868212c2a1d93b46c801eedefa5c19aa551
widya Yasa <widyarusmananda15@gmail.com>
2024-05-03 11:18:43 +0800

    change get view to card

--- src/views/templates/pages/Blogs/CreateBlog.vue
+++ src/views/templates/pages/Blogs/CreateBlog.vue
@@ -1,13 +1,39 @@
 <script setup>
 
 import BaseInput from "@/components/BaseInput.vue";
+import BaseTextArea from "@/components/BaseTextArea.vue";
+import {reactive} from "vue";
+const blogData = reactive({
+  blogName: '',
+  blogImage: '',
+  blogContent: ''
+})
+
+const previewPhoto = (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    blogData.blogImage = reader.result;
+
+    e.target.value = "";
+  };
+  reader.readAsDataURL(file);
+}
+const createBlog = () => {}
 </script>
 
 <template>
   <h1 class="text-center text-2xl font-semibold ">Create New Blog</h1>
-  <div class="mt-5 grid grid-cols-1">
+  <form  class="mt-5 grid grid-cols-1 gap-5">
+    <div class="img-upload-wrapper relative">
+      <div class="absolute">
+        <img :src="blogData.blogImage" id="file-preview" class="aspect-video object-cover" alt="">
+      </div>
+      <input type="file" class=" w-full py-[100px] opacity-0 relative" id="file-input" @change="previewPhoto">
+    </div>
     <BaseInput input-type="text" input-placeholder="enter blog title" input-title="Blog Title" input-name="blogTitle" />
-  </div>
+    <BaseTextArea input-type="text" input-placeholder="enter blog content" input-title="Blog Content" input-name="blogContent" />
+  </form>
 </template>
 
 <style scoped>
